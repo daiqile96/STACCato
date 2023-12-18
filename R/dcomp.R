23f6fda65371799da5988dc945046fd136b2e31d
Qile Dai <55097180+daiqile96@users.noreply.github.com>
2023-12-18 11:32:37 -0500

    update functions

--- R/dcomp.R
+++ R/dcomp.R
@@ -1,6 +1,54 @@
 library(tensorregress)
 library(dplyr)
 library(grid)
+library(pracma)
+library(parallel)
+library(reticulate)
+library(reshape2)
+library(CJIVE)
+
+####. Chordal Distance #####
+compare_norm = function(res1, res2){
+  
+  w2.norm = chord.norm.diff(res1$W$W2, res2$W$W2)
+  w3.norm = chord.norm.diff(res1$W$W3, res2$W$W3)
+  w4.norm = chord.norm.diff(res1$W$W4, res2$W$W4)
+  
+  norm = c(w2.norm, w3.norm, w4.norm)
+  
+  return(norm)
+}
+
+#### GSEA ############
+
+GSEA_by_cell_pair = function(sender, receiver, pathways,
+                             minSize,
+                             maxSize,
+                             nPerm){
+  
+  eff_by_cell_pair = disease_eff %>% 
+    filter(Sender == sender & Receiver == receiver) %>% 
+    select(LR, effect)
+  
+  eff = eff_by_cell_pair$effect
+  names(eff) = eff_by_cell_pair$LR
+  
+  fgseaRes <- fgsea(pathways = pathways, 
+                    stats    = eff,
+                    minSize  = minSize,
+                    maxSize  = maxSize,
+                    nPermSimple = nPerm)
+  
+  fgseaRes = fgseaRes[, c('pathway', 'pval', 'NES')] %>% 
+    mutate(sender = sender,
+           receiver = receiver)
+  
+  fgseaRes = fgseaRes[complete.cases(fgseaRes)]
+  
+  return(fgseaRes)
+  
+}
+
 
 ##### composition of cell factors #########
 
@@ -29,8 +77,7 @@ plot_sender_receiver = function(dcomp_res, cell_level, cell_label){
 }
 
 
-plot_cell_score = function(mode, dcomp_res, n, 
-                           cell_level, cell_label){
+plot_cell_score = function(mode, dcomp_res, n, cell_level, cell_label){
   
   score.mat = dcomp_res$W[[mode]]
   cells = row.names(score.mat)
@@ -48,10 +95,9 @@ plot_cell_score = function(mode, dcomp_res, n,
     if (i <= ncol(score.mat)){
       tmp.sender.score = score.mat[,i]
       p_list[[i]] = data.frame(score = unlist(tmp.sender.score),
-                               cells = cells) %>%
-        mutate(cells = factor(cells, 
-                             cell_level,
-                             cell_label)) %>% 
+                               cells = factor(cells, 
+                                              levels = cell_level,
+                                              labels = cell_label)) %>% 
         arrange(cells) %>% 
         ggplot(aes(x=1:length(tmp.sender.score), 
                    y=score,
@@ -83,11 +129,13 @@ plot_cell_score = function(mode, dcomp_res, n,
 }
 
 ##### get disease effects and pvalues ######
+
+
 get_eff_sig = function(eff_pval_df, eff_cutoff, pval_cutoff){
   
   
   eff_pval_df = eff_pval_df %>% 
-    filter(pval_cutoff <= 5e-02 & abs(effect) >= eff_cutoff) %>% 
+    filter(pval_cutoff <= pval_cutoff & abs(effect) >= eff_cutoff) %>% 
     select(LR, Sender, Receiver, effect) %>% 
     mutate(LR = as.character(LR),
            Sender = as.character(Sender),
@@ -202,7 +250,7 @@ get_contribution = function(comp, mode, cov, dcomp_res){
     W1_sub = W1[-comp, ]
     lizt = list(W1_sub, W2, W3, W4)
     C_ts_sub = ttl(as.tensor(G), lizt, ms = c(1,2,3,4))
-    U_sub = ttm(C_ts_sub, cov.ind[, -comp], m = 1)
+    U_sub = ttm(C_ts_sub, cov[, -comp], m = 1)
     
   } else if (mode == 'W2'){
     
@@ -211,7 +259,7 @@ get_contribution = function(comp, mode, cov, dcomp_res){
     
     lizt = list(W1, W2_sub, W3, W4)
     C_ts_sub = ttl(as.tensor(G_sub), lizt, ms = c(1,2,3,4))
-    U_sub = ttm(C_ts_sub, cov.ind, m = 1)
+    U_sub = ttm(C_ts_sub, cov, m = 1)
     
   } else if (mode == 'W3'){
     
@@ -220,7 +268,7 @@ get_contribution = function(comp, mode, cov, dcomp_res){
     
     lizt = list(W1, W2, W3_sub, W4)
     C_ts_sub = ttl(as.tensor(G_sub), lizt, ms = c(1,2,3,4))
-    U_sub = ttm(C_ts_sub, cov.ind, m = 1)
+    U_sub = ttm(C_ts_sub, cov, m = 1)
     
   } else if (mode == 'W4'){
     
@@ -229,7 +277,7 @@ get_contribution = function(comp, mode, cov, dcomp_res){
     
     lizt = list(W1, W2, W3, W4_sub)
     C_ts_sub = ttl(as.tensor(G_sub), lizt, ms = c(1,2,3,4))
-    U_sub = ttm(C_ts_sub, cov.ind, m = 1)
+    U_sub = ttm(C_ts_sub, cov, m = 1)
     
   }
   
@@ -238,20 +286,30 @@ get_contribution = function(comp, mode, cov, dcomp_res){
   
 }
 
-get_contribution_by_mode = function(mode, cov, dcomp_res){
+get_contribution_by_mode = function(mode, dcomp_res){
   
-  r_mode = ncol(dcomp_res$W[[mode]])
+  cov = dcomp_res$X_covar1
   
-  contr = sapply(1:r_mode, 
+  if (mode == 'cov'){
+    r_mode = 2:ncol(cov)
+    names = colnames(cov)[r_mode]
+  } else {
+    r_mode = 1:(ncol(dcomp_res$W[[mode]]))
+    names = factor(paste0('Factor', r_mode))
+  }
+
+  contr = sapply(r_mode, 
                  get_contribution, 
                  mode = mode, 
                  cov = cov, 
                  dcomp_res = dcomp_res)
   
-  contr_df = data.frame(var = factor(paste0('Factor', 1:r_mode)),
+  contr_df = data.frame(var = names,
                         mse = contr)
   
-  if (mode == 'W3'){
+  if (mode == 'cov'){
+    title = 'Sample-level Variables'
+  } else if (mode == 'W3'){
     title = 'Sender Cell Types'
   } else if (mode == 'W4'){
     title = 'Receiver Cell Types'
@@ -275,7 +333,7 @@ staccato = function(tsr,
                     lr.names = NULL, 
                     sender.names = NULL, 
                     receiver.names = NULL,
-                    core_shape, niter=20){
+                    core_shape){
   
   # initial: "random" for random initialization; "QR_tucker" for QR-based tucker initialization
   Y_1 = unfold(tsr, row_idx = 1, col_idx = c(2,3,4))@data
@@ -316,7 +374,7 @@ staccato = function(tsr,
   U = ttl(C_ts, list(X_covar1, X_covar2, X_covar3, X_covar4),c(1,2,3,4))
   sigma_est=mean((tsr@data-U@data)^2)
   lglk=dnorm(tsr@data,U@data,sqrt(sigma_est),log=TRUE)
-  dimnames(C_ts@data) <- list(colnames(cov.ind),
+  dimnames(C_ts@data) <- list(colnames(X_covar1),
                          lr.names,
                          sender.names,
                          receiver.names)
@@ -325,6 +383,7 @@ staccato = function(tsr,
   rownames(W4) = receiver.names
   
   return(list(W = list(W1 = W1,W2 = W2,W3 = W3, W4 = W4),
+              X_covar1 = X_covar1,
               G = G@data,U=U@data, 
               C_ts = C_ts@data,
               lglk = lglk, 
@@ -363,7 +422,7 @@ cal_eigen_varexp = function(tensor, mode0, modes, varexp){
 }
 
 ##### bootstrap ########
-boot = function(idx, dcomp_res, cov){
+boot = function(idx, dcomp_res){
   
   set.seed(idx)
   U = dcomp_res$U
@@ -375,26 +434,24 @@ boot = function(idx, dcomp_res, cov){
   # parametric bootstrap 
   new.residuals =  array(rnorm(U, sd = sd), dim = d)
   new.Y = as.tensor(U + new.residuals)
-  new.res = staccato(new.Y, cov, 
-                     core_shape=r, 
-                     niter=100)
+  new.res = staccato(new.Y, dcomp_res$X_covar1, 
+                     core_shape=r)
   
   return(abs(new.res$C_ts - B) > abs(B))
   
 }
 
-boot_p = function(n_boot, dcomp_res, cov, n_thread,
+boot_p = function(n_boot, dcomp_res, n_thread,
                   lr.names, sender.names, receiver.names){
   
   boot.res = mclapply(1:n_boot, 
                       boot, 
                       dcomp_res = dcomp_res, 
-                      cov = cov,
                       mc.cores = n_thread)
   
   boot.p = (Reduce('+', boot.res) + 1) / (n_boot + 1)
   
-  dimnames(boot.p) = list(colnames(cov),
+  dimnames(boot.p) = list(colnames(dcomp_res$X_covar1),
                           lr.names,
                           sender.names,
                           receiver.names)
@@ -402,3 +459,76 @@ boot_p = function(n_boot, dcomp_res, cov, n_thread,
   return(boot.p)
   
 }
+
+
+sim_comm_tsr = function(seed=NA, 
+                        whole_shape = c(20,20,20,20), 
+                        core_shape = c(3,3,3,3),
+                        p1,
+                        dup, 
+                        signal){
+  
+  d1 = whole_shape[1] ; d2 = whole_shape[2] ; d3 = whole_shape[3]  ; d4 = whole_shape[4]
+  r1 = core_shape[1] ; r2 = core_shape[2] ; r3 = core_shape[3]; r4 = core_shape[4]
+  p1 = p1; 
+  
+  #### warning for r should larger than 0
+  if(r1<=0 | r2 <= 0|r3<= 0|r4<= 0){
+    warning("the rank of coefficient tensor should be larger than 0",immediate. = T)
+    return()
+  }
+  
+  if(p1 > d1){
+    warning("the number of covariates at each mode should be no larger than the dimension of the tensor",immediate. = T)
+  }
+  
+  #### warning for p should larger than r
+  if(p1<r1 & p1>0){
+    warning("the rank of coefficient tensor should be no larger than the number of covariates",immediate. = T)
+  }
+  
+  ####-------- generate data
+  if(is.na(seed)==FALSE) set.seed(seed)
+  X_covar1 = X_covar2 = X_covar3 = X_covar4 =  NULL
+  
+  if(p1<=0){
+    X_covar1=diag(1,d1)
+    p1=d1
+  }else{
+    X_covar1 = matrix(rnorm(d1*p1,mean = 0, sd = 1/sqrt(d1)),d1,p1)
+  }
+
+  #### if use block, p and r should larger than 1 of smaller than 0
+  W1 =as.matrix(randortho(p1)[,1:r1])
+  A =  ## factor matrix
+  
+  W2 = as.matrix(randortho(d2)[,1:r2]) ## factor matrix
+  
+  W3 = as.matrix(randortho(d3)[,1:r3]) ## factor matrix
+  
+  W4 = as.matrix(randortho(d4)[,1:r4]) ## factor matrix
+  
+  ### G: core tensor
+  G = as.tensor(array(runif(r1*r2*r3*r4,min=-1,max=1),dim = c(r1,r2,r3,r4)))
+  
+  ### U: linear predictor
+  U = ttl(G,list(X_covar1%*%W1,W2,W3,W4), ms = c(1,2,3,4))@data
+  
+  ## rescale to entrywise constraint
+  G=G/max(abs(U))*signal 
+  U=U/max(abs(U))*signal
+  
+  ## C_ts: coefficient
+  C_ts=ttl(G,list(W1,W2,W3,W4),ms = c(1,2,3,4))@data 
+  
+  ### tsr:
+  tsr = lapply(seq(dup), function(x) array(rnorm(d1*d2*d3*d4,U),dim = c(d1,d2,d3,d4)))
+
+  return(list(tsr = tsr,
+              X_covar1 = X_covar1, 
+              W = list(W1 = W1, W2 = W2, W3 = W3, W4 = W4), 
+              G=G@data, 
+              U=U,
+              C_ts=C_ts))
+}
+
